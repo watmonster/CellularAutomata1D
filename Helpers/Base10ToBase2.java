420a43c7d57e8189b97c52584f0877cce7c0c754
watmonster <112777372+watmonster@users.noreply.github.com>
2022-10-08 15:53:48 -0700

    decimal to binary conversion working

---
+++ Helpers/Base10ToBase2.java
@@ -0,0 +1,7 @@
+package Helpers;
+
+public class Base10ToBase2{
+    public static String toBinaryString(Integer b10int){
+        return Integer.toBinaryString(b10int);
+    }
+}
---
+++ Helpers/HelpersMain.java
@@ -0,0 +1,7 @@
+package Helpers;
+
+public class HelpersMain {
+    public static void main(String[] args){
+        System.out.println(Base10ToBase2.toBinaryString(1234)); 
+    }
+}
